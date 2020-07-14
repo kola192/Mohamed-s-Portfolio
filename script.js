586dc014570a689e1f5d75874fcde02f949cf2bd
kola192 <mutwakil96@gmail.com>
2020-07-14 22:00:08 +0200

    final touches

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-//Global Variables
+/*
+====================== 
+Global Variables Start
+======================
+*/
+const aboutImgs = ['imgs/about1.jpg', 'imgs/about2.jpg', 'imgs/about3.jpg']
+const aboutSlideCount = aboutImgs.length
+let i = 0
+
 const navLinks = document.querySelectorAll('.nav-link')
 let slideImgs = document.querySelectorAll('.work-img')
 let slideCount = slideImgs.length
-let currentSlide = 1
+
+let sliderArray = [...slideImgs]
+
+let currentSlide = 0
+let sliderBigImg = document.querySelector('.big-img')
+
 
 let prevBtn = document.querySelector('.overlay-prev')
 let nextBtn = document.querySelector('.overlay-next') 
-console.log(nextBtn)
 
+/*
+===================== 
+Global Variables End
+=====================
+*/
+
+
+/*
+================ 
+Hamburger Start
+================
+*/
 
 //Hamburger
 document.querySelector('.hamburger').addEventListener('click', () => {
@@ -22,17 +46,69 @@ navLinks.forEach(link => {
 	link.addEventListener('click', hamburger)
 })
 
-//Hamburger Function
-function hamburger() {
-	document.body.classList.remove('open')
-		document.querySelector('.hamburger').classList.remove('toggle')
-}
+/*
+============== 
+Hamburger End
+==============
+*/
+
+
+/*
+================== 
+About Slider Start
+==================
+*/
+
+document.querySelector('.prev').addEventListener('click', () => {
+	if(i <= 0) {
+		i = (aboutSlideCount - 1)
+		aboutChecker()
+	} else {
+		i-= 1
+		aboutChecker()
+	}
+})
+
+document.querySelector('.next').addEventListener('click', () => {
+	if(i >= (aboutSlideCount - 1)) {
+		i = 0
+		aboutChecker()
+	} else {
+		i+= 1
+		aboutChecker()
+	}
+})
+
+/*
+================ 
+About Slider End
+================
+*/
+
+
+/*
+============= 
+Slider Start
+=============
+*/
 								  
 //Slider Showing & Disappering
-slideImgs.forEach(img => {
+sliderArray.forEach(img => {
+
 	img.addEventListener('click', () => {
+		currentSlide = img.dataset.slide
+
+		// Get img src
+		let imgSrc = img.getAttribute('src')
+
+		//Using The Attribute We Get In The Slider Image
+		sliderBigImg.setAttribute('src', imgSrc)
+
+		//Make Sure The Sidebar returns Off Screen
 		hamburger()
 		document.body.classList.toggle('overlay-open')
+
+		//Making a Cross Sign To Close The Slider
 		document.querySelector('.hamburger').style.display = 'none'
 		let cross = document.createElement('span')
 		cross.setAttribute('class', 'cross-sign')
@@ -42,45 +118,84 @@ slideImgs.forEach(img => {
 			document.body.classList.remove('overlay-open')
 			document.querySelector('.hamburger').style.display = 'block'
 		})
+
+		
 	})
-})
 
-// prevBtn.addEventListener('click', prevSlide)
-// nextBtn.addEventListener('click', nextSlide)
+})	
 
-// //Prev & Next Slide Functions
-// function nextSlide() {	
-// 	if(currentSlide >= slidesCount) { 
-// 			currentSlide = 1
-// 			cheker()
-// 		 } else {
-// 			 currentSlide++;
-// 			 cheker();
-// 		 }
-// } 
+prevBtn.addEventListener('click', prevSlide)
+nextBtn.addEventListener('click', nextSlide)
 
-// function prevSlide() {
-// 	if(currentSlide <= 1) {
-// 		currentSlide = slidesCount;
-// 		cheker();
-// 		 } else {
-// 			 currentSlide--;
-// 		   cheker();
-// 		 }
-// } 
-
-// //Checker Function
-// function checker() {
-
-// }
+/*
+=========== 
+Slider End
+===========
+*/
 
 
+/*
+================ 
+Functions Start
+================
+*/
 
+//Prev Slide Functions
+function prevSlide() {
+	if(currentSlide <= 0) {
+		currentSlide = slideCount-1
+		checker()
+		 } else {
+			 currentSlide--;
+		   checker()
+		 }
+}
 
+//next Slide Functions
+function nextSlide() {	
+	if(currentSlide >= (slideCount-1)) { 
+			currentSlide = 0
+			checker()
+		 } else {
+			 currentSlide++
+			 checker();
+		 }
+}  
 
+//Checker Function
+function checker() {
+	removeActive()
+	sliderArray[currentSlide].classList.add('change-imge')
 
+	let imgSrc = sliderArray[currentSlide].getAttribute('src')
 
+	//Using The Attribute We Get In The Slider Image
+	sliderBigImg.setAttribute('src', imgSrc)
+}
 
+//Remove Active Class
+function removeActive() {
+	
+	sliderArray.forEach((img) => {
+		
+		img.classList.remove('change-imge')
+		
+	})
+}	
 
+// Hamburger Function
+function hamburger() {
+	document.body.classList.remove('open')
+	document.querySelector('.hamburger').classList.remove('toggle')
+}
 
+//About Checker Function
+function aboutChecker() {
+	document.querySelector('.about-img').setAttribute('src', aboutImgs[i])
+}
 
+/*
+============== 
+Functions End
+==============
+*/
